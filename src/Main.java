f0cd0aa53cffdf5a456be7d9d54154d01cc777ed
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-09-01 21:45:43 +0200

    laver geint

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,37 +10,56 @@ public class Main {
         String svar;
 
 
-        while (true) {
-            switch (getString("hvad ønsker du")) {
+//        while (true) {
+//            switch (getString("hvad ønsker du")) {
+//
+//                case "opkonto":
+//                    break;
+//                    // her skal ske et eller andet
+//                case "findkonto":
+//                    break;
+//                    // her skal noget andet
+//                case "hæv":
+//                    break;
+//                    // sket et eller andet
+//                case "indsæt":
+//                    break;
+//                    // hvad skal der mon sker her
+//
+//                case "skift sprog":
+//                    // skrift
+//            }
+//        }
 
-                case "opkonto":
-                    break;
-                    // her skal ske et eller andet
-                case "findkonto":
-                    break;
-                    // her skal noget andet
-                case "hæv":
-                    break;
-                    // sket et eller andet
-                case "indsæt":
-                    break;
-                    // hvad skal der mon sker her
 
-                case "skift sprog":
-                    // skrift
-            }
-        }
-
+        System.out.println(getInt("skriv et tal"));
 
     }
     public static String getString(String s) {
 
-        return "fucking klovn";
+        System.out.println(s + " : ");
+        Scanner scanner = new Scanner(System.in);
+
+
+        return scanner.nextLine();
     }
 
     public static int getInt(String s) {
 
+        int i = 0;
 
-        return 666;
+        while (true) {
+            try {
+                i = Integer.parseInt(getString(s));
+                return i;   // springe linjen over ved pars fejl.
+            } catch (NumberFormatException e) {
+                System.out.println("husk det skal være et tal");
+            }
+        }
+
+
+
+
+
     }
 }
