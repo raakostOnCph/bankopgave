4f1904bd7de62d278c95a2b9c3c9d1fa6eb9345a
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-09-01 21:35:50 +0200

    lidt skitse på main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,43 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+
+        String svar;
+
+
+        while (true) {
+            switch (getString("hvad ønsker du")) {
+
+                case "opkonto":
+                    break;
+                    // her skal ske et eller andet
+                case "findkonto":
+                    break;
+                    // her skal noget andet
+                case "hæv":
+                    break;
+                    // sket et eller andet
+                case "indsæt":
+                    break;
+                    // hvad skal der mon sker her
+
+                case "skift sprog":
+                    // skrift
+            }
+        }
+
+
+    }
+    public static String getString(String s) {
+
+        return "fucking klovn";
+    }
+
+    public static int getInt(String s) {
+
+
+        return 666;
     }
 }
